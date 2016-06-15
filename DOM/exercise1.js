757de5024bbd9f7b31dc9f425973fb3a598427e9
Andrew Southard <andrewsouthard111@gmail.com>
2016-06-14 21:38:20 -0400

    DOM exercises

---
+++ DOM/exercise1.js
@@ -0,0 +1,8 @@
+
+var pSelectorOne = document.getElementById("first");
+
+var pSelectorTwo = document.querySelector("p");
+
+var pSelectorThree = document.querySelector(".special");
+
+var pSelectorFour = document.getElementByClassName("special")[0]
